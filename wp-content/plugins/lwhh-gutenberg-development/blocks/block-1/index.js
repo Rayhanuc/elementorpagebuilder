e63f747006d5a32bba515c616767a5509601616b
[Rayanuc] <[rayhanctg4@gmail.com]>
2020-03-05 09:30:41 +0600

    ৩.৪ - জাভাস্ক্রিপ্ট এপিআই ফাংশন - registerBlockType
    ৩.৪ - জাভাস্ক্রিপ্ট এপিআই ফাংশন - registerBlockType

---
+++ wp-content/plugins/lwhh-gutenberg-development/blocks/block-1/index.js
@@ -0,0 +1,127 @@
+// ; (function (wp) {
+
+//     var el = wp.element.createElement;
+//     var registerBlockType = wp.blocks.registerBlockType;
+//     var __ = wp.i18n.__;
+//     var blockStyles = {
+//         backgroundColor: '#0073aa',
+//         color: 'white',
+//         padding: '2em 2.5em',
+//         textAlign: 'center'
+//     };
+
+//     registerBlockType ( 'lwhhgd/hello-world', {
+
+//         /*
+//         Block Title
+//         */
+
+//         title: __( 'হ্যালো ওয়ার্লড' ),
+
+//          /*
+//         Block description
+//         */
+//        description: __('হ্যালো ওয়ার্লড একটি যুগান্তকারি বার্তা'),
+
+//          /*
+//         Block icon
+//         */
+//        icon: 'admin-site',
+
+//          /*
+//         Block category
+//         */
+//        category: 'common', // common / formatting / widgets / embed
+
+//          /*
+//         Block keywords helps to search the block
+//         */
+//        keywords: [ __('lwhh'), __('hello world'), __('test block') ],
+
+         
+//         // Block attribute or fields map
+        
+//        attributes: {
+//             heading: {
+//                 type: 'string',
+//                 source: 'text',
+//                 selector: 'h2',
+//                 default: __('Hello World!') // set default value
+//             },
+
+//             excerpt: {
+//                 type: 'string',
+//                 source: 'text',
+//                 selector: 'p',
+//                 default: __('This is a simple except to show how the default property works')
+//             },
+//        },
+
+//         /*
+//         Block edit function works on editor
+//         */
+
+//         edit: function(props){
+//             console.log(props);
+
+//             // var setAttributes = props.setAttributes;
+//             // var attributes = props.attributes;
+//             // var className = props.className;
+
+//             return el('h2', {style: blockStyles}, 'Hello World!')
+//         },
+
+//         /*
+//         Block save function works for output
+//         */
+
+//         save: function(props){
+//             console.log(props);
+
+//             // var setAttributes = props.setAttributes;
+//             // var attributes = props.attributes;
+
+//             return el('h2', {style: blockStyles}, 'Hello World!')
+//         },
+
+//     });
+
+// })();
+
+
+
+
+// class ৩.৪ - জাভাস্ক্রিপ্ট এপিআই ফাংশন - registerBlockType
+; (function (wp) {
+const { createElement } = wp.element;
+const { registerBlockType } = wp.blocks;
+
+const { __ } = wp.i18n;
+
+registerBlockType('lwhhgd/hello-world',
+{
+    title: __('Hi! World'),
+    description: __('This is Hello World Block'),
+    category: 'common',
+    keyworld: [
+        __('hello world'),
+        __('dummy block'),
+        __('lwhh')
+    ],
+    attributes: {},
+    style: [
+        {},
+        {}
+    ],
+    icon: 'admin-site',
+    transforms: {},
+    support: {},
+    edit() {
+        return createElement('h2', {}, 'Hello World');
+    },
+    save() {
+        return createElement('h2', {}, 'Hello World is coming from save function.');
+    }
+});
+
+}(window.wp));
---
+++ wp-content/plugins/lwhh-gutenberg-development/lwhh-gutenberg-development.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * 
+ * Plugin Name: LWHH Gutenberg Development
+ * Plugin URI:  https:
+ * Description: Demonstration plugin to learn WordPress gutenberg development with LWHH
+ * Version: 0.0.1     
+ * Author:  LWHH
+ * Author URI: https:
+ * License:     GPLv2 or later
+ * Text Domain: lwhhgd
+ * Domain Path: /languages/
+ */
+
+// if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+defined('ABSPATH' ) || exit;
+
+require plugin_dir_path(__FILE__)  . 'blocks/block-1/index.php';
+
+// require_once(plugin_dir_path( __FILE__ )."blocks/block-1/index.php");
+
+
+
+
+function lwhhgd_enqueue_block_assets() {
+    wp_enqueue_style(
+        'lwhhgd-block-1',
+        plugins_url('blocks/block-1/style.css', __FILE__),
+        array('wp-edit-blocks')
+    );
+
+
+    if(is_admin()){
+        wp_enqueue_style(
+            'lwhhgd-block-1-editor',
+            plugins_url('blocks/block-1/editor.css', __FILE__),
+            array('wp-edit-blocks')
+        );
+    }
+}
+
+// It works in front and back both side
+add_action('enqueue_block_assets', 'lwhhgd_enqueue_block_assets');
+// It will work only in editor / backend side
+// add_action('enqueue_editor_block_assets', '');
